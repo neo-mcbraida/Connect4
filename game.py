280c9eafb2c9b337fd27d18da21d4f5ec34b72f1
neo-mcbraida <56191826+neo-mcbraida@users.noreply.github.com>
2021-04-02 16:42:53 +0100

    game works

--- game.py
+++ game.py
@@ -10,29 +10,23 @@ class Grid:
                     [0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0]]
         self.won = False
-        self.reward = 0
-        self.current_inrow = 0
 
-    def Drop(self, column):
+    def Drop(self, column): 
         i = 0
         for i in range(6):
-           if self.grid[column][i] != 0:
+            if self.grid[column][i] != 0:
                 self.grid[column][i-1] = self.num
                 break
-           elif i == 5:
+            elif i == 5:
                 self.grid[column][i] = self.num
                 
-
-                
-
     def CheckDiagonal(self, row, column, inrow, left):#left = -1, right = 1, up 1, down -1
         column += left
         row -= 1
 
-        if column < 7 and row < 6:
+        if column < 7 and column > -1 and row < 6 and row > -1:
             if self.grid[column][row] == self.num:
                 inrow += 1
-                self.Update_inrow(inrow)
                 if inrow == 4:
                     self.won = True
                 else:
@@ -44,7 +38,6 @@ class Grid:
         if row != -1:
             if self.grid[column][row] == self.num:
                 inrow += 1
-                self.Update_inrow(inrow)
                 if inrow == 4:
                     self.won = True
                 else:
@@ -56,15 +49,10 @@ class Grid:
         if column != 7:
             if self.grid[column][row] == self.num:
                 inrow += 1
-                self.Update_inrow(inrow)
                 if inrow == 4:
                     self.won = True
                 else:
                     self.CheckHorizontal(row, column, inrow)
-
-    def Update_inrow(self, inrow):
-        if inrow > self.current_inrow:
-            self.current_inrow = inrow
 
 
     def PieceWinCheck(self):
@@ -93,65 +81,16 @@ class Grid:
         print(grid[0][5], grid[1][5], grid[2][5], grid[3][5], grid[4][5], grid[5][5], grid[6][5])
 
     def Turn(self):
-        self.current_inrow = 0
-        column = int(input("1-7: "))
-        column -= 1
+        column = (int(input("enter a column"))) - 1
         self.Drop(column)
         self.Display()
         self.PieceWinCheck()
+        print(self.won)
         if self.won == True:
             print("player ", self.num, " has won")
-            self.Reset(False)
         else:
-            print(self.current_inrow)
             self.SwapTurn()
             self.Turn()
             
-
-    def Grid_Reset(self):
-        for i in range(7):
-            for u in range(6):
-                self.grid[i][u] = 0
-
-        self.won = False
-        self.inrow = 0
-        self.reward = 0
-
-    def Reset(self, action):
-        if action == True:
-            self.action()
-        else:
-            Turn()
-
-    
-    def Calc_Reward(self):
-        if self.won == True:
-            self.reward = 12
-        elif self.current_inrow == 2:
-            self.reward = 1
-        elif self.current_inrow == 3:
-            self.reward = 4 
-        
-
-    def Action(self, column):
-        self.current_inrow = 0
-        column -= 1
-        self.Drop(column)
-        self.Display()
-        self.PieceWinCheck()
-        self.Calc_Reward()
-        if self.won == True:
-            print("player ", self.num, " has won")
-            self.Grid_Reset()
-            return self.grid
-            return self.reward
-            self.Reset(True)
-        else:
-            return self.reward
-            self.SwapTurn()
-            self.action()
-        
-        
-            
-episode = Grid()
-#episode.Turn()
+game = Grid()
+game.Turn()
